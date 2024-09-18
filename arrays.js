cb33bb8fb71aca6955f40e83b8855f884ec0adc8
TRPLX_Gaming <icgabriel2007@gmail.com>
2024-09-18 06:15:35 +0100

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -13,8 +13,23 @@ Array.prototype.increaseAll = function(factor=1) {
 
 //extracts the highest numeric value in an array
 Array.prototype.getHighest = function() {
-  let arr = this;
-  let h1 = arr[0];
-  
-
+  let arr = this
+  let h1 = arr[0],
+     h2 = arr[1];
+  for(let n=0;n<arr.length;n++) {
+    if(typeof arr[n] != "number") {
+      throw "Invalid data type detected!"
+    }
+    if(h1<h2) {
+      let sub = h1
+      h1 = h2
+      h2 = sub
+    }
+    if(h1 < arr[n]) {
+      h1 = arr[n]
+    } else if(h2 < arr[n] && arr[n] != h1) {
+      h2 = arr[n]
+    }
+  }
+  return `${h1}`
 }
